835cbda82f5b43ab8eec667a51fd9a3a10deab30
JohnHed <johnhed@live.com>
2019-10-08 17:25:40 -0300

    .

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,34 +1,36 @@
 <nav class="pcoded-navbar">
     <div class="nav-list">
         <div class="pcoded-inner-navbar main-menu">
-            <div class="pcoded-navigation-label">Navigation</div>
-            <ul class="pcoded-item pcoded-left-item">
-                <li class="pcoded-hasmenu active pcoded-trigger">
-                    <a href="javascript:void(0)" class="waves-effect waves-dark">
-                        <span class="pcoded-micon"><i class="feather icon-home"></i></span>
-                        <span class="pcoded-mtext">Dashboard</span>
-                    </a>
-                    <ul class="pcoded-submenu">
-                        <li class="active">
-                            <a href="index.html" class="waves-effect waves-dark">
-                                <span class="pcoded-mtext">Default</span>
-                            </a>
-                        </li>
-                        <li class="">
-                            <a href="dashboard-crm.html" class="waves-effect waves-dark">
-                                <span class="pcoded-mtext">CRM</span>
-                            </a>
-                        </li>
-                        <li class="">
-                            <a href="dashboard-analytics.html" class="waves-effect waves-dark">
-                                <span class="pcoded-mtext">Analytics</span>
-                                <span class="pcoded-badge label label-info ">NEW</span>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
+            @foreach (menu() as $item)
+                    <ul class="pcoded-item pcoded-left-item">
+                        
+                @if(isset($item['submenu']))
+                            <li class="pcoded-hasmenu pcoded-trigger">
+                                <a href="#" class="waves-effect waves-dark">
+                                    <span class="pcoded-micon"><i class="{{$item['icon']}}"></i></span>
+                                    <span class="pcoded-mtext">{{$item['text']}}</span>
+                                </a>
+                                <ul class="pcoded-submenu">
+                                    @foreach ($item['submenu'] as $item)
+                                        <li>
+                                            <a href="{{route($item['url'])}}" class="waves-effect waves-dark">
+                                                <span class="pcoded-micon"><i class="{{$item['icon']}}"></i></span>
+                                                <span class="pcoded-mtext">{{$item['text']}}</span>
+                                            </a>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </li>
+                @else
+                    <li>
+                        <a href="{{route($item['url'])}}" class="waves-effect waves-dark">
+                            <span class="pcoded-micon"><i class="{{$item['icon']}}"></i></span>
+                            <span class="pcoded-mtext">{{$item['text']}}</span>
+                        </a>
+                    </li>
+                @endif
             </ul>
-
+            @endforeach
         </div>
     </div>
 </nav>
